07de51d48d4bb761c0ed1de8b1793f2c737bb2f8
bistik <bistik@protonmail.ch>
2015-04-22 14:41:19 +0800

    display a sample usage line if not enough command line arguments are given to the script

--- wack/grep_range.pl
+++ wack/grep_range.pl
@@ -1,30 +1,45 @@
 #!/usr/bin/env perl
 
-open my $fh, $ARGV[0] or die "Can't open $ARGV[0] or maybe there is no argument? $!";
+main();
 
-my ($out, $is_grabbing) = ('', 0);
+sub main {
+        my $file_to_grep = @ARGV[0];
+        my $start = quotemeta $ARGV[1];
+        my $end = quotemeta $ARGV[2];
 
-my $start = quotemeta $ARGV[1];
-my $end = quotemeta $ARGV[2];
+        unless ($file_to_grep) {
+                usage();
+                exit 0;
+        }
 
-unless ($start or $end) {
-        print "no start and end patterns provided";
-        exit;
+        unless ($start or $end) {
+                usage();
+                exit 0;
+        }
+
+        open my $fh, $file_to_grep or die "Can't open $file_to_grep: $!";
+
+        my ($out, $is_grabbing) = ('', 0);
+
+
+
+
+        while (my $line = <$fh>) {
+                if ($line =~ /$start/ and !$is_grabbing) {
+                        $is_grabbing = 1;
+                        $out .= $line;
+                        next;
+                }
+
+                $out .= $line if $is_grabbing;
+
+                last if $line =~ /$end/ and $is_grabbing;
+        }
+
+        print $out;
 }
 
-
-while (my $line = <$fh>) {
-    if ($line =~ /$start/ and !$is_grabbing) {
-        $is_grabbing = 1;
-        $out .= $line;
-        next;
-    }
-
-    $out .= $line if $is_grabbing;
-
-    if ($line =~ /$end/ and $is_grabbing) {
-        last;
-    }
+sub usage {
+        print "\nUsage:\n";
+        print "\tperl $0 <file_to_grep> <start_pattern> <end_pattern>";
 }
-
-print $out;
