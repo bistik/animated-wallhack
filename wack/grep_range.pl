6a6c497bc43e133ceb288efcfcab2161a82e2ebb
bistik <bistik@protonmail.ch>
2015-04-22 16:01:36 +0800

    cleanup the tabs. some lines have 8space tabs. everything should be 4space tabs. Edit usage text to make it more clear

--- wack/grep_range.pl
+++ wack/grep_range.pl
@@ -1,45 +1,46 @@
 #!/usr/bin/env perl
 
+use Term::ANSIColor;
+
 main();
 
 sub main {
-        my $file_to_grep = @ARGV[0];
-        my $start = quotemeta $ARGV[1];
-        my $end = quotemeta $ARGV[2];
+    my $file_to_grep = @ARGV[0];
+    my $start = quotemeta $ARGV[1];
+    my $end = quotemeta $ARGV[2];
 
-        unless ($file_to_grep) {
-                usage();
-                exit 0;
+    unless ($file_to_grep) {
+        usage();
+        exit 0;
+    }
+
+    unless ($start or $end) {
+        usage();
+        exit 0;
+    }
+
+    open my $fh, $file_to_grep or die "Can't open $file_to_grep: $!";
+
+    my ($out, $is_grabbing) = ('', 0);
+
+    while (my $line = <$fh>) {
+        if ($line =~ /$start/ and !$is_grabbing) {
+                $is_grabbing = 1;
+                $out .= $line;
+                next;
         }
 
-        unless ($start or $end) {
-                usage();
-                exit 0;
-        }
+        $out .= $line if $is_grabbing;
 
-        open my $fh, $file_to_grep or die "Can't open $file_to_grep: $!";
+        last if $line =~ /$end/ and $is_grabbing;
+    }
 
-        my ($out, $is_grabbing) = ('', 0);
-
-
-
-
-        while (my $line = <$fh>) {
-                if ($line =~ /$start/ and !$is_grabbing) {
-                        $is_grabbing = 1;
-                        $out .= $line;
-                        next;
-                }
-
-                $out .= $line if $is_grabbing;
-
-                last if $line =~ /$end/ and $is_grabbing;
-        }
-
-        print $out;
+    print $out;
 }
 
 sub usage {
-        print "\nUsage:\n";
-        print "\tperl $0 <file_to_grep> <start_pattern> <end_pattern>";
+    print "\nUsage: ./$0 file start end";
+    print "\n   file: file to grep";
+    print "\n  start: pattern to mark start of search";
+    print "\n    end: pattern to mark end of search\n";
 }
