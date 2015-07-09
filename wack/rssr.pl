defed991b732ef13efaffc8e8895628f7a821179
Erick <bistik@protonmail.ch>
2015-07-09 10:04:39 +0000

    the start of an awesome rss reader...

---
+++ wack/rssr.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use feature 'say';
+use LWP::UserAgent;
+use XML::RSS;
+use Data::Dumper;
+
+my $ua = LWP::UserAgent->new;
+my @uris = qw(
+http://reddit.com/r/perl/.rss
+http://reddit.com/r/nba/.rss
+http://www.spin.ph/rss
+https://news.ycombinator.com/rss
+);
+
+#my $response = $ua->get("http://www.spin.ph/rss");
+foreach my $uri (@uris) {
+    say "[$uri]";
+    my $response = $ua->get($uri);
+    if ($response->is_success) {
+        my $rss = XML::RSS->new;
+        $rss->parse($response->decoded_content);
+        while (my $item = shift @{$rss->{'items'}}) {
+                say join ", ", keys $item;
+                say $item->{title}, "\n";
+                #say $item->{link}, "\n";
+        }
+    } else {
+        print $response->status_line;
+    }
+}
