419a4a1083f3a1ff595a2d4c69dc78b48f373bcf
Erick <bistik@protonmail.ch>
2015-07-22 00:45:51 +0800

    continue reading chapter 3

--- hello_mixu/ch3/server.js
+++ hello_mixu/ch3/server.js
@@ -8,9 +8,27 @@ var messages = ["testing"];
 var clients  = [];
 
 http.createServer(function (req, res) {
-    fs.readFile('./index.html', function(err, data) {
-        res.end(data);
-    });
-    //res.end("Hello world");
+    var url_parts = url.parse(req.url);
+    if (url_parts.pathname === '/') {
+        fs.readFile('./index.html', function(err, data) {
+            res.end(data);
+        });
+    } else if (url_parts.pathname.substr(0, 5) == '/poll') {
+        var count = url_parts.pathname.replace(/[^0-9]*/, '');
+        console.log(count);
+        if (messages.length > count) {
+            res.end(JSON.stringify(
+                {
+                    count: messages.length,
+                    append: messages.slice(count).join("\n") + "\n"
+                }
+            ));
+        } else {
+            clients.push(res);
+        }
+        // polling code here
+    } else {
+        res.end("404");
+    }
 }).listen(port, 'localhost');
-console.log('Server running.');
+console.log('Server running at port ' + port);
