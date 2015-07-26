fad3ad3205b448c20acca4fab4f5b4d8fe460c17
Erick <bistik@protonmail.ch>
2015-07-27 00:26:40 +0800

    now we start to read the rss file and use feedparser to read items

--- hello_rss/sample_feedparser.js
+++ hello_rss/sample_feedparser.js
@@ -1,6 +1,7 @@
 var FeedParser = require('feedparser');
 var request = require('request');
 var fs = require('fs');
+var path = require('path');
 
 var feeds = [
   {
@@ -15,7 +16,8 @@ var node_feed = feeds[0];
 var req = request(node_feed.url, function(error, response, body) {
   if (!error && response.statusCode === 200 ) {
     console.log("call write_feed_to_file");
-    write_feed_to_file(node_feed.name, body);
+    //write_feed_to_file(node_feed.name, body);
+    read_feed_from_file(node_feed.name)
   }
 });
 
@@ -23,20 +25,40 @@ var req = request(node_feed.url, function(error, response, body) {
 //        - check if file exists?
 //        - check if content is text?
 //        - check if content is xml?
-function write_feed_to_file(filename, content) {
+function write_feed_to_file(feed, content) {
   console.log("will attempt to write feed to a file... say your prayers...");
-  var path = 'feeds/' + filename + '.xml'; // TODO surely must be a module that constructs file paths...
+  var fullpath = path.join(__dirname, 'feeds', feed + '.xml');
   fs.writeFile(path, content, function (error) {
     if (error) {
       console.log(error);
     } else {
-      console.log("RSS feed: " + path + " was saved! More hard drive clutter, hooray!");
+      console.log("RSS feed: " + fullpath + " was saved! More hard drive clutter, hooray!");
     }
   });
 }
 
-// TODO: implement, unit tests
-function load_feed_file(filename) {
-  //
+// TODO:
+// unit tests
+// don't know how to unit test in node/electron
+function read_feed_from_file(feed) {
+  var feedparser = new FeedParser();
+  var fullpath = path.join(__dirname, 'feeds', feed + '.xml');
+  fs.createReadStream(fullpath)
+    .on('error', function (error) {
+      console.error(error);
+    })
+    .pipe(new FeedParser())
+    .on('error', function (error) {
+      console.error(error);
+    })
+    .on('meta', function (meta) {
+      console.log('==== %s ====', meta.title);
+    })
+    .on('readable', function() {
+      var stream = this;
+      var item;
+      while (item = stream.read()) {
+        console.log('Got article: %s', item.title || item.description);
+      }
+    });
 }
-//var feedparser = new FeedParser();
