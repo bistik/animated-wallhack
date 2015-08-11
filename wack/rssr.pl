8c3356cea373988395bf52224d9406414431dcf7
Erick <bistik@protonmail.ch>
2015-08-11 01:49:41 +0000

    cleanup

--- wack/rssr.pl
+++ wack/rssr.pl
@@ -11,21 +11,22 @@ my $ua = LWP::UserAgent->new;
 my @uris = qw(
 http://reddit.com/r/perl/.rss
 http://reddit.com/r/nba/.rss
+http://reddit.com/r/racket/.rss
 http://www.spin.ph/rss
 https://news.ycombinator.com/rss
 );
 
-#my $response = $ua->get("http://www.spin.ph/rss");
 foreach my $uri (@uris) {
     say "[$uri]";
     my $response = $ua->get($uri);
+
     if ($response->is_success) {
         my $rss = XML::RSS->new;
         $rss->parse($response->decoded_content);
         while (my $item = shift @{$rss->{'items'}}) {
-                say join ", ", keys $item;
-                say $item->{title}, "\n";
-                #say $item->{link}, "\n";
+            #say join ", ", keys $item;
+            say $item->{title}, "\n";
+            say $item->{link}, "\n";
         }
     } else {
         print $response->status_line;
