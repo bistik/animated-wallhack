ae62f85c7cd3208c9113e06e259c24496d61cd71
bistik <bistik@protonmail.ch>
2015-10-05 12:05:13 +0800

    dotes dotes

--- counter/counter.pl
+++ counter/counter.pl
@@ -2,15 +2,27 @@
 use strict;
 use warnings;
 
+use LWP::Simple;
+
+my $hero_list_page = get("http://www.dotabuff.com/heroes");
+if ($hero_list_page =~ /(<div class="hero-grid">.*?<p class="footnote">.*?<\/div>)/s ) {
+    $hero_list_page = $1;
+}
+
+print $hero_list_page;
+
 my @draft = @ARGV;
 
 my %counter = (
     'cm', [qw(am hus)],
     'am', [qw(riki lc)],
+    'pl', [qw(es lc)],
+    'pudge', [qw(ls necro)],
+    'riki', [qw(bh slardar)],
 );
 
 foreach my $h (@draft) {
-        if (exists($counter{$h})) {
-                print "$h : @{$counter{$h}}\n";
-        }
+    if (exists($counter{$h})) {
+        print "$h : @{$counter{$h}}\n";
+    }
 }
