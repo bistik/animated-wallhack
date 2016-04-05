2dc829413b5e200d17678942f557249a7a60272a
Erick <bistik@protonmail.ch>
2016-04-06 00:00:56 +0800

    youtoob code math

---
+++ code_math_js/ep3/trig02.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		width	= canvas.width = window.innerWidth,
+		height  = canvas.height = window.innerHeight;
+
+	var centerY = height * .5,
+		centerX = width * .5,
+		speed   = 0.1,
+		offset  = height * .4,
+		angle   = 0;
+
+	render();
+
+	function render() {
+		var y = centerY + Math.sin(angle) * offset;
+
+		context.clearRect(0, 0, width, height);
+		context.beginPath();
+		context.arc(centerX, y, 50, 0, Math.PI * 2, false);
+		context.fill();
+
+		angle += speed;
+
+
+		/*
+		(function animloop(){
+  			requestAnimFrame(animloop);
+  			render();
+		})();
+		*/
+		requestAnimationFrame(render);
+	}
+
+	//setInterval(render, 10);
+};
